bf17dae111f180dee840042cbc35ea02458db734
Pace Davis <pacemdavis@gmail.com>
2020-07-21 10:27:46 -0500

    trying to launch

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 49050
 
 app.use(bodyParser.json())
 app.use(
